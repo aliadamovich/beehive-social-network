a2039b734be20d7b7a21ee8f41600e4bd5658832
aliadamovich <alesya.argentina@gmail.com>
2024-08-21 00:36:39 -0300

    bug fixing modal

--- src/components/layout/GalleryPage/Gallery.tsx
+++ src/components/layout/GalleryPage/Gallery.tsx
@@ -19,61 +19,56 @@ import 'swiper/css/navigation';
 
 
 export const Gallery = () => {
+	
 	const [activePhotoIndex, setActivePhotoIndex] = useState(1);
 	const [isModalOpen, setIsModalOpen] = useState(false);
-
 	const photoGrid = useSelector(getPhotoGrid);
-	const swiper = useSwiper();
+
 	const openPhoto = (photoIndex: number) => {
 		setActivePhotoIndex(photoIndex);
 		setIsModalOpen(true)
 	}
 
 	return (
-		<>
-			<StyledGallerySection>
-				{/* {isModalOpen && <StyledBackGround onClick={() => { setIsModalOpen(false) }}></StyledBackGround>} */}
-				<Container>
 
-							<Search />
-							<StyledGalleryContent>
-								<StyledPhotosContainer>
-										<PhotoGrid photoGrid={photoGrid} openPhoto={openPhoto}/>
-								</StyledPhotosContainer>
-								<Recent />
-							</StyledGalleryContent>
+		<StyledGallerySection>
+			<Container>
+				<Search />
+				<StyledGalleryContent>
+					<StyledPhotosContainer>
+						<PhotoGrid photoGrid={photoGrid} openPhoto={openPhoto} />
+					</StyledPhotosContainer>
+					<Recent />
+				</StyledGalleryContent>
+			</Container>
 
-				</Container>
 			{isModalOpen &&
-				<StyledModalWindow>
+				<StyledModalWindow onClick={() => { setIsModalOpen(false) }}>
 
-					<Swiper
-						initialSlide={activePhotoIndex}
-						slidesPerView={1}
-						spaceBetween={10}
-						navigation={{
-							nextEl: '.swiper-button-next',
-							prevEl: '.swiper-button-prev',
-						}}
-						// pagination={{ el: '.swiper-pagination', clickable: true }}
-						// navigation={{
-						// 	nextEl: '.swiper-button-next',
-						// 	prevEl: '.swiper-button-prev',
-						// }}
-						modules={[Pagination, Navigation]}
-						className={s.slider}>
-						{photoGrid.map(p => <SwiperSlide key={p.id} className={s.slide}><img src={p.photo} alt="user's photo" /></SwiperSlide>)}
+					<ModalContent onClick={(e) => { e.stopPropagation() }}>
 
-						<CustomSwiperButtonPrev><GrLinkPrevious /></CustomSwiperButtonPrev>
-						<CustomSwiperButtonNext><GrLinkNext /></CustomSwiperButtonNext>
-					</Swiper>
-					
-					<StyledClose onClick={() => { setIsModalOpen(false) }}><VscChromeClose /></StyledClose>
-				</StyledModalWindow>}
-				<StyledBackGround onClick={() => { setIsModalOpen(false) }}></StyledBackGround>
-			</StyledGallerySection>
+						<Swiper
+							initialSlide={activePhotoIndex}
+							slidesPerView={1}
+							spaceBetween={10}
+							navigation={{
+								nextEl: '.swiper-button-next',
+								prevEl: '.swiper-button-prev',
+							}}
+							modules={[Pagination, Navigation]}
+							className={s.slider}>
+							{photoGrid.map(p => <SwiperSlide key={p.id} className={s.slide}><img src={p.photo} alt="user's photo" /></SwiperSlide>)}
 
-		</>
+							<CustomSwiperButtonPrev><GrLinkPrevious /></CustomSwiperButtonPrev>
+							<CustomSwiperButtonNext><GrLinkNext /></CustomSwiperButtonNext>
+						</Swiper>
+
+						<StyledClose onClick={() => { setIsModalOpen(false) }}><VscChromeClose /></StyledClose>
+					</ModalContent>
+				</StyledModalWindow>
+			}
+
+		</StyledGallerySection>
 	)
 }
 
@@ -81,7 +76,6 @@ export const Gallery = () => {
 const StyledGallerySection = styled.section`
 
 `
-
 
 const StyledGalleryContent = styled.div`
 	display: flex;
@@ -108,27 +102,28 @@ const StyledModalWindow = styled.div`
 	left: 0;
 	z-index: 2003;
 	overflow: hidden;
+	background-color: #1b1b1bc0;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	
+`
+const ModalContent = styled.div`
+	width: 90%;
+	max-width: 1000px;
+	padding: 10px;
+	background-color: #fff;
+	border-radius: 4px;
 `
 
-const StyledBackGround = styled.div`
-	position: absolute;
-	width: 100%;
-	height: 100%;
-	top: 0;
-	left: 0;
-	bottom: 0;
-	right: 0;
-	background-color: #1b1b1b94;
-	z-index: 2002;
-`
 
 const StyledClose = styled.button`
 	position: absolute;
-	top: 10%;
-	right: 10%;
+	top: 5%;
+	right: 5%;
 	border: none;
 	border-radius: 50%;
-	background: linear-gradient(to top left, rgb(189, 139, 237) 20%, rgb(129, 29, 222));
+	background-color: #ffffff82;
 	padding: 5px;
 	display: flex;
 	justify-content: center;
@@ -138,10 +133,10 @@ const StyledClose = styled.button`
 	svg{
 		width: 25px;
 		height: 25px;
-		color: #fff;
+		color: currentColor;
 	}
 	&:hover{
-		opacity: 0.8;
+		background-color: #ffffffb0;
 	}
 `
 //Swiper
